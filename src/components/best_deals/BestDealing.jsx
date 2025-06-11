022c5f92e68f3b330d093bfb6e73ea1e89bbcf0e
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-11 13:58:49 +0500

    dealing ui design

--- src/components/best_deals/BestDealing.jsx
+++ src/components/best_deals/BestDealing.jsx
@@ -5,19 +5,24 @@ import { centerData } from '../../data/data'
 const BestDealing = () => {
   return (
     <>
-      <div className="container grid grid-cols-1 md:grid-cols-3">
-        <div className="left grid grid-cols-2 gap-3">
+      <div className="container grid grid-cols-1 md:grid-cols-3 bg-gray-100">
+        <div className="left grid grid-cols-2 gap-2 cursor-pointer rounded-xl">
             {leftSideData.map((left,index)=> (
-                <div className="" key={index}>
-                    <h1>{left.brand}</h1>
-                    <h1>{left.title}</h1>
+                <div className=" space-y-3 bg-white px-2 py-3 hover:shadow-xl" key={index}>
+                    <h1 className='text-gray-500 text-sm hover:text-yellow-400'>{left.brand}</h1>
+                    <h1 className='line-clamp-1 text-blue-600 text-md font-bold hover:text-yellow-400'>{left.title}</h1>
+                    <img className='w-36 h-36 sm:w-64 sm:h-64 md:w-32 md:h-32 object-cover' src={left.image} alt="" />
+                    <div className="cart flex justify-between items-center pr-4">
+                        <h1 className='text-gray-700 text-lg sm:text-xl md:text-sm'>{left.price}</h1>
+                        <p>cart</p>
+                    </div>
                 </div>
             ))}
         </div>
 
         <div className="center flex flex-col items-center">
             {centerData.map((center,i)=> (
-                <div className="" key={i}>
+                <div className="bg-white" key={i}>
                     <h1>{center.brand}</h1>
                     <h1>{center.title}</h1>
                     <img className='w-80 object-cover' src={center.image} alt="" />
@@ -28,7 +33,7 @@ const BestDealing = () => {
 
         <div className="right grid grid-cols-2 gap-2">
             {rightSideData.map((right,indx)=> (
-                <div className="border space-y-3" key={indx}>
+                <div className="border space-y-3 bg-white" key={indx}>
                     <h1>{right.brand}</h1>
                     <h1>{right.title}</h1>
                     <img className='w-40 border' src={right.image} alt="" />
--- src/components/best_deals/Deals.jsx
+++ src/components/best_deals/Deals.jsx
@@ -5,11 +5,11 @@ import BestDealing from './BestDealing'
 const Deals = () => {
   return (
     <>
-      <div className="bg-gray-100 mt-6 pb-4">
+      <div className="bg-gray-100 mt-6 pb-4 px-3">
         <DealsTab className="flex justify-between space-x-4 sm:justify-center items-center sm:space-x-1 md:space-x-10  px-1 sm:px-4 py-5"/>
-        <hr className='bg-gray-100 h-[1px] mx-3 sm:mx-5 md:mx-28'/>
-      </div>
+        <hr className='bg-gray-100 h-[1px] mx-3 sm:mx-5 md:mx-28 mb-4'/>
       <BestDealing/>
+      </div>
     </>
   )
 }
