433e35103ad6247b55e59c32e2721b5b26947025
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-14 16:14:49 +0500

    working

--- src/components/best_sellers/BestSelling.jsx
+++ src/components/best_sellers/BestSelling.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { sellingData } from '../../data/data'
 const BestSelling = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-4'>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-3 my-3'>
       {sellingData.map((sellItem, index)=> (
-        <div className="flex flex-row items-center" key={index}>
+        <div className="flex flex-row items-center border" key={index}>
           <div className="img">
-            <img className='w-68' src={sellItem.image} alt={sellItem.title} />
+            <img className='w-32' src={sellItem.image} alt={sellItem.title} />
           </div>
-          <div className="headings space-y-12">
+          <div className="headings space-y-12 px-2 py-3">
             <div className="topHead">
               <h1>{sellItem.brand}</h1>
           <h1>{sellItem.title}</h1>
