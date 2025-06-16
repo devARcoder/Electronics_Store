f87d48db875d1caabd7dbe30f27d1263a5bc0874
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-16 09:39:22 +0500

    borders

--- src/components/new_arrivals/NewArrivals.jsx
+++ src/components/new_arrivals/NewArrivals.jsx
@@ -6,7 +6,7 @@ const NewArrivals = () => {
     {/* flex-shrink-0 */}
     <div className="flex overflow-x-auto no-scrollbar gap-4 mx-4 md:mx-26 mb-6">
         {newArrivalsData.map((arrival, i)=> (
-            <div className="flex-shrink-0 w-[40%] md:w-[22%]" key={i}>
+            <div className="flex-shrink-0 w-[50%] md:w-[22%] border-r border-gray-200" key={i}>
                 <h1 className="text-gray-500 text-sm hover:text-yellow-400">{arrival.brand}</h1>
                 <h1 className='line-clamp-1 text-blue-600 text-md font-bold hover:text-yellow-400'>{arrival.title}</h1>
 
