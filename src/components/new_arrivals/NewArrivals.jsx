66676e6d92a4d16baa1a90eb2aec710aefd3c7f2
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-16 09:07:20 +0500

    issues solve

--- src/components/new_arrivals/NewArrivals.jsx
+++ src/components/new_arrivals/NewArrivals.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const NewArrivals = () => {
   return (
     <>
-    <div className="flex overflow-x-auto no-scrollbar gap-4 mx-4 mb-6">
+    <div className="flex overflow-x-auto no-scrollbar gap-4 mx-4 md:mx-26 mb-6">
         <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
         <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
         <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
