a39f143bb8682b8c43bfdc2da1d64ba256d2fae2
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-16 09:01:45 +0500

    chnges

--- src/components/new_arrivals/Arrivals.jsx
+++ src/components/new_arrivals/Arrivals.jsx
@@ -1,26 +1,19 @@
 import React from 'react'
 import Heading from '../shared/common/Heading'
-import ArrivalsTab from './ArrivalsTab'
+import NewArrivals from './NewArrivals'
 const Arrivals = () => {
   return (
-    <div className='px-4 py-4 md:px-24'>
-      <div className="">
+    <>
+    <div className='px-4 py-8 md:px-24'>
+      
 
-      <div className="head md:flex justify-between items-center md:pt-6">
-        <div className="heading py-3 ">
-            <Heading name="New Arrivals"/>
-            <div className="border-b-3 border-yellow-400 md:border-none w-24 pt-2"></div>
-            <div className="line border-b-1 border-gray-400 md:border-none"></div>
-        </div>
-        <div className="tabs flex space-x-4">
-            <ArrivalsTab className="md:space-x-6"/>
-        </div>
-      </div>
-      <div className="hidden md:inline-block md:border-b md:bg-gray-400 h-[1px] w-full mb-10"></div>
+    <Heading name="New Arrivals" className="my-2"/>
+    <div className="border-b-3 border-yellow-400 w-28"></div>
+    
+    <hr className='text-gray-200'/>
     </div>
-
-
-    </div>
+    <NewArrivals/>
+    </>
   )
 }
 
---
+++ src/components/new_arrivals/NewArrivals.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+const NewArrivals = () => {
+  return (
+    <>
+    <div className="flex overflow-x-auto no-scrollbar gap-4 mx-4 mb-6">
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+        <div className="flex-shrink-0 w-44 h-44 bg-gray-100"></div>
+    </div>
+    </>
+  );
+};
+
+export default NewArrivals;
