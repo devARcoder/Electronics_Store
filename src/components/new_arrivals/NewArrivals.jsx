d4add6559f4dfdfba8722da05ddb2b0353c087fa
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-16 09:45:49 +0500

    borders and paddings will solved

--- src/components/new_arrivals/NewArrivals.jsx
+++ src/components/new_arrivals/NewArrivals.jsx
@@ -6,7 +6,7 @@ const NewArrivals = () => {
     {/* flex-shrink-0 */}
     <div className="flex overflow-x-auto no-scrollbar gap-4 mx-4 md:mx-26 mb-6">
         {newArrivalsData.map((arrival, i)=> (
-            <div className="flex-shrink-0 w-[50%] md:w-[22%] border-r border-gray-200" key={i}>
+            <div className="flex-shrink-0 w-[50%] md:w-[15.5%] border-b border-r border-t border-gray-200 cursor-pointer hover:shadow-xl px-3 py-3" key={i}>
                 <h1 className="text-gray-500 text-sm hover:text-yellow-400">{arrival.brand}</h1>
                 <h1 className='line-clamp-1 text-blue-600 text-md font-bold hover:text-yellow-400'>{arrival.title}</h1>
 
