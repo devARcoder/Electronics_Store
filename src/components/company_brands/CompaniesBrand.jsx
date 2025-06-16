8d089289a4f490f8a80f37a2021f9e1abfab312f
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-16 10:59:08 +0500

    completed

--- src/components/company_brands/CompaniesBrand.jsx
+++ src/components/company_brands/CompaniesBrand.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { companiesBrand } from '../../data/data'
 const CompaniesBrand = () => {
   return (
-    <div className='flex justify-between items-center border-b border-t border-gray-200 py-2 px-3 mx-4 md:mx-24 my-9'>
+    <div className='flex justify-between items-center border-b border-t border-gray-200 py-6 sm:py-2 sm:px-3 mx-4 md:mx-24 my-9'>
       {companiesBrand.map((brand, i)=> (
         <div key={i}>
             <img src={brand.image} alt="" />
