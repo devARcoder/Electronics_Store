46f311a9a6ebef50b2943e7fb6cfbf86e3d29235
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-16 09:32:32 +0500

    mapping data

--- src/components/new_arrivals/NewArrivals.jsx
+++ src/components/new_arrivals/NewArrivals.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { newArrivalsData } from "../../data/data";
 const NewArrivals = () => {
   return (
     <>
     {/* flex-shrink-0 */}
     <div className="flex overflow-x-auto no-scrollbar gap-4 mx-4 md:mx-26 mb-6">
-        
+        {newArrivalsData.map((arrival, i)=> (
+            <div className="flex-shrink-0 w-[40%] md:w-[22%]" key={i}>
+                <h1>{arrival.brand}</h1>
+                <h1>{arrival.title}</h1>
+
+                <img src={arrival.image} alt={arrival.title} />
+
+                <div className="flex space-x-2">
+                    <h1>{arrival.price}</h1>
+                    <h1>{arrival.oldPrice}</h1>
+                </div>
+            </div>
+        ))}
     </div>
     </>
   );
