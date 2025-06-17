7866484979f2123bfd5c19e4a969f05c54f7e0a2
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-17 15:36:37 +0500

    some bugs fix

--- src/components/company_brands/CompaniesBrand.jsx
+++ src/components/company_brands/CompaniesBrand.jsx
@@ -5,7 +5,7 @@ const CompaniesBrand = () => {
     <div className='flex justify-between items-center border-b border-t border-gray-200 py-6 sm:py-2 sm:px-3 mx-4 md:mx-24 my-9'>
       {companiesBrand.map((brand, i)=> (
         <div key={i}>
-            <img src={brand.image} alt="" />
+            <img className='cursor-pointer' src={brand.image} alt="" />
         </div>
       ))}
     </div>
