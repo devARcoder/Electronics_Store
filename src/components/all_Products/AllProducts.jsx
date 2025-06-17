d06c102d651963eda41ea577eefc75d808cd7e8e
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-17 12:05:25 +0500

    mapping it

--- src/components/all_Products/AllProducts.jsx
+++ src/components/all_Products/AllProducts.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Heading from '../shared/common/Heading'
+import { featuredProducts } from '../../data/data'
 const AllProducts = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-4 gap-3 mx-4 my-6'>
@@ -7,6 +8,11 @@ const AllProducts = () => {
         <Heading name="Featured Products" className="my-2"/>
     <div className="border-b-3 border-yellow-400 w-28"></div>
     <hr className='text-gray-200'/>
+    {featuredProducts.map((featured, i)=> (
+        <div className="" key={i}>
+            
+        </div>
+    ))}
       </div>
       <div className="onsale">
         <Heading name="Onsale Products" className="my-2"/>
