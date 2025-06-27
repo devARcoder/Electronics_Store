61ac0d4430f054e2e373858eac8d287d9a524c41
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-27 11:07:57 +0500

    changes

--- src/components/collections/AllCollections.jsx
+++ src/components/collections/AllCollections.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Collections from './Collections'
+import FeatureDetails from '../featured/FeatureDetails'
 const AllCollections = () => {
   return (
     <>
+    <FeatureDetails/>
       <Collections/>
       <AllProducts />
     </>
