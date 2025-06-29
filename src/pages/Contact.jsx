4650948833fbe1fef474e42677f09707f7dcb852
Abdur Razzaq <devarcoder789@gmail.com>
2025-06-29 16:46:59 +0500

    changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,17 +15,19 @@ const Contact = () => {
       <h1 className='border-b-2 border-yellow-400 text-xl text-gray-700 font-semibold w-44 pb-3'>Leave A Message</h1>
       <hr className='text-gray-300'/>
       <p className='text-gray-600 py-3'>If you have any questions please send us a message using the adjacent form and we will get back to you as soon as possible</p>
-      
+
     </div>
     <div className="form flex flex-col space-y-2 mt-5">
-      <label htmlFor="Name" className='font-bold text-gray-700 pt-3'>Name</label>
+      <div className="">
+        
       <Input type="text" className="border border-gray-200 py-3 px-3 rounded-full"/>
-      <label htmlFor="Name" className='font-bold text-gray-700 pt-3'>Email</label>
+      
       <Input type="email" className="border border-gray-200 py-3 px-3 rounded-full"/>
-      <label htmlFor="Name" className='font-bold text-gray-700 pt-3'>Phone Number</label>
+      </div>
+      
       <Input type="phone" className="border border-gray-200 py-3 px-3 rounded-full"/>
-      <label htmlFor="Name" className='font-bold text-gray-700 pt-3'>Message</label>
-      <textarea name="" id="" rows={7} className='border border-gray-200 py-3 px-3 rounded-2xl'></textarea>
+      
+      <textarea placeholder='Message' name="" id="" rows={7} className='border border-gray-200 py-3 px-3 rounded-2xl'></textarea>
     </div>
     </div>
   )
